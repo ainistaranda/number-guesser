783061e4bf36724e8ad2ad4700d57183c743108f
Ainis Taranda <taranda.ainis@gmail.com>
2022-10-13 10:38:10 -0400

    guessing

--- guess.js
+++ guess.js
@@ -8,3 +8,37 @@ if it's the right number say "congratulations" and exit
 
 import readline from "readline-sync"
 
+let userGuessedRight = false
+
+// while(true){ // me playing
+// guess a number from 1-10
+let numberChosen = Math.ceil(Math.random() * 10)
+// console.log(numberChosen)
+// }
+while (!userGuessedRight) {
+    let guess = readline.question("What is your guess?")
+    if (guess == numberChosen) {
+        console.log("Congratulations")
+        userGuessedRight = true
+    }
+    if (guess > numberChosen) {
+        console.log("too high! try again")
+        // userGuessedRight = true
+    }
+    if (guess < numberChosen) {
+        console.log("too low! try again")
+        // userGuessedRight = true
+    }
+    
+
+    // console.log(guess)
+}
+// while(!userGuessedRight) {
+//     console.log("What is your guess?")
+// }
+
+
+
+
+// make NaN if
+
